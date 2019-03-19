b3769e33f5ff8b6740fa6da599186626a6899210
Stefano Gatti <stefano.gatti4@studio.unibo.it>
2019-03-19 16:33:48 +0100

    QoL sui led

--- src/main/java/appl/MainSegChainFramework.java
+++ src/main/java/appl/MainSegChainFramework.java
@@ -24,20 +24,20 @@ public class MainSegChainFramework extends SegChainFramework {
         actor1.setLedModel(ledModel.getChannel());
         chainSystem.addChainLink(actor1);
         chainSystem.addLedModel(ledModel);
-        chainSystem.addConcreteLed(chainSystem.getLinkAt(0), new LedSegmentAdapter("seg1", 20, 10, 1000, 0));
+        chainSystem.addConcreteLed(chainSystem.getLinkAt(0), new LedSegmentAdapter("seg1", 20, 10, 100, 0));
 
         //the creation of the LedModels can be left to the framework
         chainSystem.addChainLink(new ChainLinkActor("seg2", 500));
-        chainSystem.addConcreteLed(chainSystem.getLastLink(), new LedSegmentAdapter("seg2", 20, 10, 1120, 0));
+        chainSystem.addConcreteLed(chainSystem.getLastLink(), new LedSegmentAdapter("seg2", 20, 10, 250, 0));
 
         chainSystem.addChainLink(new ChainLinkActor("seg3", 500));
-        chainSystem.addConcreteLed(chainSystem.getLastLink(), new LedSegmentAdapter("seg3", 20, 10, 1240, 0));
+        chainSystem.addConcreteLed(chainSystem.getLastLink(), new LedSegmentAdapter("seg3", 20, 10, 400, 0));
 
         chainSystem.addChainLink(new ChainLinkActor("seg4", 500));
-        chainSystem.addConcreteLed(chainSystem.getLastLink(), new LedSegmentAdapter("seg4", 20, 10, 1360, 0));
+        chainSystem.addConcreteLed(chainSystem.getLastLink(), new LedSegmentAdapter("seg4", 20, 10, 550, 0));
 
         chainSystem.addChainLink(new ChainLinkActor("seg5", 500));
-        chainSystem.addConcreteLed(chainSystem.getLastLink(), new LedSegmentAdapter("seg5", 20, 10, 1480, 0));
+        chainSystem.addConcreteLed(chainSystem.getLastLink(), new LedSegmentAdapter("seg5", 20, 10, 700, 0));
     }
 
     public static void main(String[] args) {
@@ -50,13 +50,13 @@ public class MainSegChainFramework extends SegChainFramework {
         for (int k = 1; k < 2; k++) {
             for (int i = 0; i < 5; i++) {
                 chainSystem.addChainLink(new ChainLinkActor("seg"+i+""+k, 500));
-                chainSystem.addConcreteLed(chainSystem.getLastLink(), new LedSegmentAdapter("seg"+i+""+k, 20, 10,1000+120*i,300*k));
-                Utils.delay(2000);
+                chainSystem.addConcreteLed(chainSystem.getLastLink(), new LedSegmentAdapter("seg"+i+""+k, 20, 10,100+150*i,200*k));
+                Utils.delay(1000);
             }
         }
         System.out.println("-----------------------TESTING START/STOP----------------------------");
         MsgUtil.INSTANCE.forward(MsgUtil.INSTANCE.startMsg(), chainSystem.getFirstLink().getChannel());
-        Utils.delay(6000);
+        Utils.delay(10000);
         MsgUtil.INSTANCE.forward(MsgUtil.INSTANCE.stoptMsg(), chainSystem.getFirstLink().getChannel());
     }
 
